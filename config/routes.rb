05acded6e5ce85c96d7032e94db2bd84988000ec
jade prieboy <jadeprieboy@gmail.com>
2014-11-21 00:39:39 +0000

    built nav bar and added welcome controller and set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/us'
+
+  get 'welcome/contact'
+
+  get 'welcome/features'
+
+  get 'welcome/faqs'
+
+  get 'welcome/pricing'
+
   resources :invoices
 
-  root to: 'invoices#index'
+  root to: 'welcome#index'
   
   
   
