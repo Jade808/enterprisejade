a2f5ef8f32395c5cc059f3727366594fa410d8fe
jade prieboy <jadeprieboy@gmail.com>
2014-11-23 06:20:08 +0000

    reconfig'd environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,16 +6,4 @@ Rails.application.initialize!
 
 
 
-Freelanceful::Application.initialize!
 
-# Configuration for using SendGrid on Heroku
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :user_name => "app31774168@heroku.com",
-  :password => "lxse1nqs",
-  :domain => "staging.freelanceful.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
