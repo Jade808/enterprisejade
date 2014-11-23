180ed9f429345b2a63a5a16e34dd044f8530dea6
jade prieboy <jadeprieboy@gmail.com>
2014-11-23 06:09:23 +0000

    added config.mailer.default in prod.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,20 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Load the rails application
-require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Freelanceful::Application.initialize!
 
-# Configuration for using SendGrid on Heroku
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :user_name => "app31774168@heroku.com",
-  :password => "lxse1nqs",
-  :domain => "staging.freelanceful.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
