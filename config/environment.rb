4454ae757e4b06e7cc123b20feece9a89b56cf79
jade prieboy <jadeprieboy@gmail.com>
2014-11-23 05:46:53 +0000

    configured environment.rb for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,21 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Freelanceful::Application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app31774168@heroku.com",
+  :password => "lxse1nqs",
+  :domain => "enterprisejade.herokuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
