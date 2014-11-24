e62e1ba7d60f729d37beed77505d4d9de9ec664d
jade prieboy <jadeprieboy@gmail.com>
2014-11-24 07:17:50 +0000

    fixed devise bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app31774168@heroku.com'],
+    :password       => ENV['lxse1nqs'],
     :domain         => 'heroku.com'
   }
 
